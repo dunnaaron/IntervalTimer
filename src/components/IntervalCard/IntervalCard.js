fb9b7faff7957ac069f9a526aa1d16d1761ece9d
Aaron Dunn <dunnac1@gmail.com>
2020-05-05 05:55:27 -0400

    added input row titles and general interval card styling

--- src/components/IntervalCard/IntervalCard.js
+++ src/components/IntervalCard/IntervalCard.js
@@ -3,7 +3,18 @@ import './interval-card.scss'
 
 const IntervalCard = () => {
     return <div className='interval-card'>
-
+        <p className='interval-card-title'>
+            Interval
+        </p>
+        <div className='input-row name'>
+            <p className='input-row-title'>Name:</p>
+        </div>
+        <div className='input-row time'>
+            <p className='input-row-title'>Time:</p>
+        </div>
+        <div className='input-row rest'>
+            <p className='input-row-title'>Rest:</p>
+        </div>
     </div>
 }
 
