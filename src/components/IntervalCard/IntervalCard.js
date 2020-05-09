5bc07cc86d8c34182f704bfeb141fb480f122d40
Aaron Dunn <dunnac1@gmail.com>
2020-05-09 10:30:34 -0400

    WIP

--- src/components/IntervalCard/IntervalCard.js
+++ src/components/IntervalCard/IntervalCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './interval-card.scss'
+import TextField from '@material-ui/core/TextField'
 
 const IntervalCard = () => {
     return <div className='interval-card'>
@@ -8,12 +9,15 @@ const IntervalCard = () => {
         </p>
         <div className='input-row name'>
             <p className='input-row-title'>Name:</p>
+            <TextField />
         </div>
         <div className='input-row time'>
             <p className='input-row-title'>Time:</p>
+            <TextField />
         </div>
         <div className='input-row rest'>
             <p className='input-row-title'>Rest:</p>
+            <TextField />
         </div>
     </div>
 }
